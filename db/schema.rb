3ea953c29b621a1378c6996eff3d2de5467ca624
David Elì <davidelimg1@gmail.com>
2020-01-10 12:16:05 -0600

    Added association between Docs and Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_050149) do
+ActiveRecord::Schema.define(version: 2020_01_10_180027) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_31_050149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_docs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
